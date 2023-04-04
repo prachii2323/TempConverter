a90b9a4ce7934e6e125463b7253c969644e80583
prachii2323 <96729272+prachii2323@users.noreply.github.com>
2023-04-04 21:52:50 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,25 @@
+var cel =  document.getElementById("cel");
+        var fah =  document.getElementById("fah");
+    
+        cel.addEventListener('input', function(){
+            let c= this.value;
+            let f=(c*9/5)+32;
+            
+            if(!Number.isInteger(f)){
+                f=f.toFixed(4);
+            }
+            fah.value=f;
+            // console.log("cel  changed");
+        });
+        fah.addEventListener('input', function(){
+            let f= this.value;
+            let c=(f-32)*5/9;
+            
+            if(!Number.isInteger(c)){
+                c=c.toFixed(4);
+
+            }
+            cel.value=c;
+
+            // console.log("Fah changed");
+        });
